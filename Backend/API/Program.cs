bb463ee73f87f320adbc5d82a29ff430612ef7ba
Jen Spencer <spe19021@byui.edu>
2023-09-20 20:25:45 -0600

    created backend

--- Backend/API/Program.cs
+++ Backend/API/Program.cs
@@ -1,25 +1,40 @@
+using API.GraphQL;
+using Core.Interfaces;
+using GraphQL.Server.Ui.Voyager;
+using Infrastructure.Data;
+using Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
+
+var AllowSpecificOrigins = "_allowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContextFactory<OMAContext>(options => {
+    options.UseInMemoryDatabase("InMemoryDb");
+});
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ICustomerService, CustomerService>();
+builder.Services.AddScoped<IOrderService, OrderService>();
+
+// GraphQl
+builder.Services
+.AddGraphQLServer()
+.AddQueryType<Query>()
+.AddFiltering();
+
+// Cors
+builder.Services.AddCors(options => {
+    options.AddPolicy(name: AllowSpecificOrigins, 
+    policy => {
+        policy.AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
+app.UseCors(AllowSpecificOrigins);
+app.MapGraphQL();
+app.UseGraphQLVoyager("/graphql-voyager", new VoyagerOptions { GraphQLEndPoint = "/graphql" });
 app.Run();
