2f32e3ed22c349fa42a842e52564e92d7a21feb2
Jen Spencer <spe19021@byui.edu>
2023-09-28 20:01:24 -0600

    set up frontend and connected with backend

---
+++ Frontend/ordermanagement/src/app/layout/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './styles.css';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import CustomersDashboard from '../../features/customers/customersDashboard/CustomersDashboard';
+const client = new ApolloClient({
+  cache: new InMemoryCache({
+    typePolicies: {},
+  }),
+  uri: 'http://localhost:5212/graphql',
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <CustomersDashboard />
+    </ApolloProvider>
+  );
+}
+
+export default App;
