7374ff5f2d6fc50112c2c17b447851879f04b0dc
Jen Spencer <spe19021@byui.edu>
2023-09-28 20:47:15 -0600

    added layout

--- Frontend/ordermanagement/src/app/layout/App.tsx
+++ Frontend/ordermanagement/src/app/layout/App.tsx
@@ -2,17 +2,27 @@ import React from 'react';
 import './styles.css';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import CustomersDashboard from '../../features/customers/customersDashboard/CustomersDashboard';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './Layout';
+import HomePage from '../../features/home/HomePage';
 const client = new ApolloClient({
   cache: new InMemoryCache({
     typePolicies: {},
   }),
-  uri: 'http://localhost:5212/graphql',
+  uri: process.env.REACT_APP_API_SCHEMA_URL,
 });
 
 function App() {
   return (
     <ApolloProvider client={client}>
-      <CustomersDashboard />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path='customers' element={<CustomersDashboard />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </ApolloProvider>
   );
 }
---
+++ Frontend/ordermanagement/src/features/home/HomePage.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function HomePage() {
+  return <h1>Welcome to Order Managment App</h1>;
+}
